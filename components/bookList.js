ad359bc26bf6791fca7bcadcfacc9602967f0190
john <kodchanat2@gmail.com>
2017-09-01 21:58:13 +0700

    remove(mock): remove mock data onload

--- components/bookList.js
+++ components/bookList.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, ListView } from 'react-native';
 import BookDetail from './bookDetail';
 
-const data = [
-    { title: "SEEING RED", author: "Sandra Brown", book_image: "https://s1.nyt.com/du/books/images/9781455572106.jpg"},
-    { title: "THE STORE", author: "James Patterson and Richard DiLallo", book_image: "https://s1.nyt.com/du/books/images/9780316395540.jpg"},
-    { title: "CAMINO ISLAND", author: "John Grisham", book_image: "https://s1.nyt.com/du/books/images/9780385543057.jpg"}
-  ];
+const data = [];
 
 class BookList extends React.Component {
     constructor(props){
