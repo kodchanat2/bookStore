9262f237a017a546fefc185832fc68e34cfd639b
john <kodchanat2@gmail.com>
2017-09-01 18:02:09 +0700

    test(data): add mock

--- components/bookList.js
+++ components/bookList.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const data = [
+    { title: "SEEING RED", author: "Sandra Brown", book_image: "https://s1.nyt.com/du/books/images/9781455572106.jpg"},
+    { title: "THE STORE", author: "James Patterson and Richard DiLallo", book_image: "https://s1.nyt.com/du/books/images/9780316395540.jpg"},
+    { title: "CAMINO ISLAND", author: "John Grisham", book_image: "https://s1.nyt.com/du/books/images/9780385543057.jpg"}
+  ];
+
 class BookList extends React.Component {
  
   render() {
