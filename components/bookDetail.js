fa388491f5aa25540e2813229dbb66341fe40a30
john <kodchanat2@gmail.com>
2017-09-01 18:10:14 +0700

    style(row view): arrange component

--- components/bookDetail.js
+++ components/bookDetail.js
@@ -5,13 +5,44 @@ class BookDetail extends React.Component {
     
     render(){
         return (
-            <View>
-                <Text>{this.props.author}</Text>
-                <Text>{this.props.title}</Text>
+            <View style={styles.container}>
+                <Image style={styles.img} source={{uri: this.props.book_image}} resizeMode='contain'/>
+                <View style={styles.description}>
+                    <Text style={styles.author}>{this.props.author}</Text>
+                    <Text style={styles.title}>{this.props.title}</Text>
+                </View>
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container:{
+        padding: 10,
+        height:110,
+        borderBottomWidth: 2,
+        borderColor: '#ddd',
+        flexDirection: 'row'
+    },
+    img:{
+        height: 90,
+        width: 80
+    },
+    description:{
+        paddingRight: 10,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+
+    },
+    title:{
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    author:{
+        fontSize: 15
+    }
+})
 
 export default BookDetail;
