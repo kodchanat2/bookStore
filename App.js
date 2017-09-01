25ec583cc78279e6fcf3e1338d92fceb267e4ec3
john <kodchanat2@gmail.com>
2017-09-01 18:29:41 +0700

    feature(nav bar): add nav bar

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import BookList from './components/bookList';
+import { Router, Scene } from 'react-native-router-flux';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <BookList/>
+        <Router>
+          <Scene key="root">
+            <Scene key="list" component={BookList} title="List" initial={true} />
+          </Scene>
+        </Router>
       </View>
     );
   }
