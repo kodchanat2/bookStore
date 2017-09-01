e8bd79a237272020f79cda8c78fefea94de3b164
john <kodchanat2@gmail.com>
2017-09-01 18:00:44 +0700

    refactor(bookList): move view to bookList

---
+++ components/bookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class BookList extends React.Component {
+ 
+  render() {
+    return (
+        <Text>Hi</Text>
+    );
+  }
+}
+
+export default BookList;
